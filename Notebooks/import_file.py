96cf00e2df3aa9327a21de117cc2eaabec080fd1
miles-frankllin <70984342+miles-frankllin@users.noreply.github.com>
2020-10-13 13:59:16 -0400

    Add files via upload
    First commit

---
+++ Notebooks/import_file.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import time
+from sklearn import tree
+from sklearn.model_selection import cross_val_score
+from IPython.display import Image
